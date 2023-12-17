f46b5dea5a725cc7cfe3656d5fcddc29539d9ad8
201li220 <150241037+201li220@users.noreply.github.com>
2023-12-17 17:24:41 +0100

    Update BERTAdapter_V1.py
    Comment of the adapter structure

--- main/modified_transformers/models/bert/BERTAdapter_V1.py
+++ main/modified_transformers/models/bert/BERTAdapter_V1.py
@@ -8,10 +8,19 @@ Created on Tue Nov 21 11:47:48 2023
 from transformers import BertForSequenceClassification, BertTokenizer,BertModel,BertConfig
 from torch import nn
 
-
+"""Adapter module using bottleneck architecture"""
 
 class Adapter(nn.Module):
-    def __init__(self,input_size, hidden_size):
+    def __init__(self,
+                 input_size, 
+                 hidden_size):
+                     
+        """Adapter config:
+        Args :
+            input_size : Size of the input
+            hidden_size : Size of the Adapter
+        """
+        
         super(Adapter,self).__init__()
         self.FeedForward_down_layer = nn.Linear(input_size, hidden_size)
         self.FeedForward_up_layer = nn.Linear(hidden_size,input_size)
@@ -20,7 +29,11 @@ class Adapter(nn.Module):
         self._initialize_weights()
 
     def _initialize_weights(self):
-        #near identity initialization
+
+        """Near zero initialization of the feedforward layers 
+        resulting in a near identity initialization of the adapter module
+        """
+        
         nn.init.normal_(self.FeedForward_down_layer.weight, mean = 0, std = 1e-2)
         nn.init.normal_(self.FeedForward_up_layer.weight, mean = 0, std = 1e-2)
         
